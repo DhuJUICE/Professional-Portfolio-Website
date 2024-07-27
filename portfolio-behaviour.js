8cf3fa69a72841688f0ab6ccb9733accd9a5d386
DhuJUICE <junierherandien84@gmail.com>
2024-07-27 21:36:48 +0200

    added button events in js file

--- portfolio-behaviour.js
+++ portfolio-behaviour.js
@@ -1,3 +1,20 @@
-const btnAbout = document.getElementById('btnAbout');
-const btnCV = document.getElementById('btnCV');
-const btnProjects = document.getElementById('btnProjects');
+const btnAbout = document.getElementById("btnAbout");
+const btnCV = document.getElementById("btnCV");
+const btnProjects = document.getElementById("btnProjects");
+const bodyText = document.getElementById("bodyText");
+
+var aboutText = "this is about me";
+var cvText = "this is my cv";
+var projectText = "this is my projects";
+
+btnAbout.addEventListener("click", function{
+    
+});
+
+btnCV.addEventListener("click", function{
+    
+});
+
+btnProjects.addEventListener("click", function{
+    
+});
