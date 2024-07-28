ca582daffe05bfb4b2e504cdf3fb15a563c6e163
DhuJUICE <junierherandien84@gmail.com>
2024-07-28 06:33:47 +0200

    added contact details
    Also edited the layout ot my portfolio website

--- portfolio-behaviour.js
+++ portfolio-behaviour.js
@@ -1,19 +1,14 @@
 const btnAbout = document.getElementById('btnAbout');
-const btnCV = document.getElementById("btnCV");
 const btnProjects = document.getElementById("btnProjects");
 const bodyText = document.getElementById("bodyText");
 
-var aboutText = "ABOUT<br>HOBBIES/INTERESTS<br>CONTACT INFO";
-var cvText = "EDUCATION<br>WORK EXPERIENCE<br>SKILLS";
+var aboutText = "ABOUT<br>HOBBIES/INTERESTS";
 var projectText = "PROJECTS<br>Project1<br>Project2";
 
 btnAbout.addEventListener('click', function(){
     bodyText.innerHTML = aboutText;
 });
 
-btnCV.addEventListener("click", function(){
-    bodyText.innerHTML = cvText;
-});
 
 btnProjects.addEventListener("click", function(){
     bodyText.innerHTML = projectText;
