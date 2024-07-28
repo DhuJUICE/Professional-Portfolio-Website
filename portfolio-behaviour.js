52e52815e341a4ec798a49c5f13c5bb1582ff48e
DhuJUICE <junierherandien84@gmail.com>
2024-07-28 10:18:09 +0200

    added my projects

--- portfolio-behaviour.js
+++ portfolio-behaviour.js
@@ -1,9 +1,10 @@
 const btnAbout = document.getElementById('btnAbout');
+const btnDetails = document.getElementById("btnDetails");
 const btnProjects = document.getElementById("btnProjects");
 const bodyText = document.getElementById("bodyText");
 
 const aboutText = `
-    <table border="1" width="100%" height="100%">
+    <table border="0" width="100%" height="100%">
         <tr>
             <td>
                 <h2>About</h2>
@@ -36,12 +37,44 @@ const aboutText = `
         </tr>
     </table>
 `;
-var projectText = "PROJECTS";
+
+const detailsText = `
+    Professional Details
+`;
+
+const projectText = `
+    <table border="0" width="100%" height="100%">
+        <tr>
+            <td align="center" width="50%">
+                <h2>Project 1</h2>
+                <h2>Ecommerce Web Application Backend</h2>
+                <p>
+                    With this project i was asked to help with the development of the backend for a ecommerce web application using the Django
+                    Web Development Framework and Python. I had to work with PostgreSQL and develop the different models for the data requirements
+                    as well as to link the backend to the PostgreSQL database for seemless interaction between database and backend using the ORM django provides.
+                    I look forward to completing the financial transaction functionality on this project using the Stripe API.
+                </p>
+            </td>
+            <td align="center" width="50%">
+                <h2>Project 2</h2>
+                <h2>Email Retrieving Automation</h2>
+                <p>
+                    This was a project i wanted to do for myself as i do not like to open my
+                    emails everytime i start up my computer, so i developed a program that will run at startup of my pc and show me all my new inbox emails
+                    without having to log in everytime. I made use of the simplegmail Gmail API. It asks you to log into your gmail account once and them stores a gmail_token to be used everytime you run the program without logging into gmail on pc startup again.
+                </p>
+            </td>
+        </tr>
+    </table>
+`;
 
 btnAbout.addEventListener('click', function(){
     bodyText.innerHTML = aboutText;
 });
 
+btnDetails.addEventListener('click', function(){
+    bodyText.innerHTML = detailsText;
+});
 
 btnProjects.addEventListener("click", function(){
     bodyText.innerHTML = projectText;
